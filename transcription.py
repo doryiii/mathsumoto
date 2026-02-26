69378ac5c2ed70ad335c2780cd7749fa3135d540
Dory <dory@dory.moe>
2026-02-26 12:36:15 -0800

    ability to ignore certain speaking users

--- transcription.py
+++ transcription.py
@@ -4,11 +4,13 @@ from discord.ext.voice_recv.extras.speechrecognition import SpeechRecognitionSin
 import speech_recognition as sr
 from speech_recognition.recognizers.whisper_api.openai import OpenAICompatibleRecognizer
 import openai
+import db
 
 
 class TranscriptionCog(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
+    self.ignored_users = {}
     self.openai_client = openai.OpenAI(
         api_key=self.bot.WHISPER_KEY,
         base_url=self.bot.WHISPER_URL
@@ -21,12 +23,18 @@ class TranscriptionCog(commands.Cog):
       await ctx.send(f"{ctx.author.name} is not in a voice channel.")
       return
 
+    guild_id = ctx.guild.id if ctx.guild else None
+    self.ignored_users[(ctx.channel.id, guild_id)] = db.get_ignored_users(
+      ctx.channel.id, guild_id)
+
     def text_callback(user, text):
       if text and text.strip():
         ctx.bot.loop.create_task(ctx.send(f"**{user.name}**: {text}"))
 
     def process_callback(recognizer, audio, user):
       if not audio.frame_data:
+        return None
+      if user.id in self.ignored_users.get((ctx.channel.id, guild_id), set()):
         return None
       try:
         return self.whisper_recognizer.recognize(audio, model="notneeded")
@@ -61,6 +69,36 @@ class TranscriptionCog(commands.Cog):
 
     vc.listen(sink)
     await ctx.send(f"Listening to #{ctx.author.voice.channel.name}...")
+
+  @commands.command()
+  async def ignoreme(self, ctx):
+    guild_id = ctx.guild.id if ctx.guild else None
+    key = (ctx.channel.id, guild_id)
+    if key not in self.ignored_users:
+      self.ignored_users[key] = db.get_ignored_users(ctx.channel.id, guild_id)
+    self.ignored_users[key].add(ctx.author.id)
+    db.add_ignored_user(ctx.channel.id, guild_id, ctx.author.id)
+    await ctx.send(f"Okay, I will ignore your voice in this channel, {ctx.author.mention}.")
+
+  @commands.command()
+  async def unignoreme(self, ctx):
+    guild_id = ctx.guild.id if ctx.guild else None
+    key = (ctx.channel.id, guild_id)
+    if key not in self.ignored_users:
+      self.ignored_users[key] = db.get_ignored_users(ctx.channel.id, guild_id)
+    self.ignored_users[key].discard(ctx.author.id)
+    db.remove_ignored_user(ctx.channel.id, guild_id, ctx.author.id)
+    await ctx.send(f"I will listen to your voice again in this channel, {ctx.author.mention}.")
+
+  @commands.command()
+  async def ignoredusers(self, ctx):
+    guild_id = ctx.guild.id if ctx.guild else None
+    users = db.get_ignored_users(ctx.channel.id, guild_id)
+    if not users:
+      await ctx.send("No users are currently being ignored in this channel.")
+      return
+    mentions = ", ".join(f"<@{u}>" for u in users)
+    await ctx.send(f"Currently ignored users in this channel: {mentions}")
 
   @commands.command(aliases=["bye", "stop"])
   async def stoptranscribe(self, ctx):
